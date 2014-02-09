54d3a0a80f5bc6e490202177a443583c231b4983
kunaltyagi <kunal.tyagi.3.1994@gmail.com>
2014-02-10 01:13:12 +0530

    side-project started

---
+++ puzzles/sudoku_solver.java
@@ -0,0 +1,19 @@
+import java.utils.*;
+
+class Sudoku{
+    public boolean fill_grid(){
+        ;
+    }
+
+    public boolean set_confidence(int level){
+        ;
+    }
+
+    public boolean check(int i, int j, int k = 0){
+        ;
+    }
+
+    public static void main(String args[]){
+        ;
+    }
+}
