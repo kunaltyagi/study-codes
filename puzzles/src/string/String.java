1d383a32e49f86a55d1152044d76d6a01b8face9
kunaltyagi <tyagi.kunal@live.com>
2014-03-23 02:58:20 +0530

    work on table started

--- puzzles/src/string/String.java
+++ puzzles/src/string/String.java
@@ -1,10 +1,22 @@
 package string;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author kunaltyagi
  */
 public class String {
+    // a 2D area of puzzle
+    private ArrayList<ArrayList<Cell>> table;
+    int n, m;
+    int type;   //0: Sudoku, 1: Skyscraper
+    
+    public String()
+    {
+        table = new ArrayList<ArrayList<Cell>>();
+    }
+    public String(int n, int m)
 
     /**
      * @param args the command line arguments
