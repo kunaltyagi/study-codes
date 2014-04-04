30aa9f5964f1b23f66d429b5cda72f085b53b462
kunaltyagi <tyagi.kunal@live.com>
2014-04-04 23:53:00 +0530

    doing the parser, some pains

--- CS213/assignment_2/tries/src/Generic/Parse.java
+++ CS213/assignment_2/tries/src/Generic/Parse.java
@@ -10,7 +10,7 @@ import tries.*;
 public class Parse {
     
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        Tries trie = new Tries();
+        //Tries trie = new Tries();
         
         FileInputStream fstream = new FileInputStream
                 ("/home/kunaltyagi/workspace/studies/study-codes/CS213/assignment_2/tries/src/penn.txt");
@@ -18,42 +18,44 @@ public class Parse {
         String line, word, type;
         word = " "; type = " ";
         while ((line = br.readLine()) != null) {
-           // process the line.
-           for(int i = 0; i < line.length(); ++i) {
-               if(line.charAt(i) == '/')
-               {
-                   if(i+1 < line.length()) {
-                       type = line.substring(i+1);
-                   }
-                   else
-                   {
+            // process the line.
+            for(int i = 0; i < line.length(); ++i) {
+                if(line.charAt(i) == '/')
+                {
+                    System.out.print(i);
+                    System.out.println(line.charAt(i));
+                    if(i+1 < line.length()) {
+                        type = line.substring(i+1);
+                    }
+                    else
+                    {
                        type = "NA";
-                   }                   
-                   if(i > 0) {
+                    }                   
+                    if(i > 0) {
                        word = line.substring(0, i-1);
-                   }
-                   else {
+                    }
+                    else {
                        word = " ";
-                   }
+                    }
                    
-                   break;
-               }
-               if(i+1 == line.length()) {
-                   word = " ";
-                   type = "NA";
-               }
-           }            
-           // generate the word
-           if(word.length() == 0)
-           {
-               word = " ";
-               type = "NA";
-           }
-           trie.add(word);
+                    break;
+                }
+                if(i+1 == line.length()) {
+                    word = " ";
+                    type = "NA";
+                }
+            }            
+            // generate the word
+            if(word.length() == 0)
+            {
+                word = " ";
+                type = "NA";
+            }
+            //trie.add(word);
         }
         br.close();
         
-        trie.print();
+        //trie.print();
     }
     
 }
